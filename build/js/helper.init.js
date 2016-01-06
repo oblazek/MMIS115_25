eb8b767d7b78bcbc9159e0ed68d9a2c0b35470c7
oblazek <ondra.blazkuj@gmail.com>
2016-01-06 19:47:40 +0100

    finished translations, comments for documentation

--- build/js/helper.init.js
+++ build/js/helper.init.js
@@ -2,12 +2,13 @@
  * Helper Module init
  *
  * @module helper-init
- * @author Ondrej Blazek, Cecilia Ritzen <oblazek@student.tugraz.at>, <critzen@student.tugraz.at>
+ * @author Ondrej Blazek <oblazek@student.tugraz.at>
+ * @author Cecilia Ritzén <critzen@student.tugraz.at>
  * @requires Widget
  * @requires Language
  *
  * @version 1.0.0
- * 
+ * @license Apache-2.0
  */
 ModularMVC.registerHelper('helper.init', function (Widget, LanguageModule) {
 	/* public methods */
@@ -20,12 +21,8 @@ ModularMVC.registerHelper('helper.init', function (Widget, LanguageModule) {
 		 * @memberOf module:helper-init
 		 */
 		onWidgetReady: function(){
-			debug("Helper.Init onWidgetReady: initialized the localization strings");
 			
 			var lang = Widget().getPreference('lang');
-			
-			debug("Helper.Init: set language preference: " + lang);
-			//debug("Helper.Init: initialize the language module now");
 			
 			LanguageModule().properties({
 				usefile: false,
@@ -40,20 +37,28 @@ ModularMVC.registerHelper('helper.init', function (Widget, LanguageModule) {
 						"RESULT": "Result!",
 						"WELCOME": "Welcome to the Geography Quiz. This application tests your European country skills. Press Start to begin the journey!",
 						"LANG": "Deutsch",
+						"TIME": "Your time: ",
+						"RIGHT_ANSWER": "Correct answer #<br>",
+						"WRONG_ANSWER": "Incorrect answer #<br>",
+						"CONGRATZ": "Congratulations! You made it through the quiz. Your result is: "
 						// "AN_ERROR_OCCURRED_PLEASE_TRY_AGAIN": "An error occurred. Please try again!",
-						"AN_UNEXPECTED_ERROR_OCCURRED.": "An unexpected error occured."
+						// "AN_UNEXPECTED_ERROR_OCCURRED.": "An unexpected error occured."
 					},
 					de: {
 						"START_BUTTON": 'Anfang',
 						"BACK": 'Zurück',
 						"NEXT": "Nächsten",
-						"QUIZ": "Die QUIZ",
+						"QUIZ": "Die Geographie-Quiz",
 						"RESULTS": "Ergebnisse",
 						"RESULT": "Ergebnis!",
 						"WELCOME": "Willkommen in der Geographie-Quiz. Diese Anwendung testet Ihre Fähigkeiten europäischen Land. Drücken Sie auf Start, um die Reise zu beginnen!",
 						"LANG": "English",
+						"TIME": "Ihr Zeit: ",
+						"RIGHT_ANSWER": "Korrekte Antwort #<br>",
+						"WRONG_ANSWER": "Falsche Antwort #<br>",
+						"CONGRATZ": "Herzliche Glückwünsche! Sie machten es durch das Quiz. Ihr Ergebnis: "
 						// "AN_ERROR_OCCURRED_PLEASE_TRY_AGAIN": "Ein Fehler ist aufgetreten. Versuche es erneut!",
-						"AN_UNEXPECTED_ERROR_OCCURRED.": 'Ein unerwarteter Fehler ist aufgetreten.'
+						// "AN_UNEXPECTED_ERROR_OCCURRED.": 'Ein unerwarteter Fehler ist aufgetreten.'
 					}
 				}
 			});
@@ -66,6 +71,5 @@ ModularMVC.registerHelper('helper.init', function (Widget, LanguageModule) {
 	
 	/* end of private methods */
 	
-	//debug('Helper.Init loaded');
 	return interFace;
 }(ModularMVC.MVCHelper('Widget'), ModularMVC.MVCHelper('Language')));
